9520065f67e5da56ea2e0303cdd3e1374015ceb0
hwangblood <hwangblood@gmail.com>
2023-06-24 21:09:05 +0800

    IDs, Scopes and Endpoints

--- lib/auth/infrastructure/github_authenticator.dart
+++ lib/auth/infrastructure/github_authenticator.dart
@@ -5,6 +5,28 @@ import 'package:oauth2/oauth2.dart' as oauth2 show Credentials;
 import 'package:repo_viewer/auth/infrastructure/credentials_storage/credentials_storage.dart';
 
 class GithubAuthenticator {
+  // TODO: don't use plain text here
+  static const clientID = '8c18a431995a651c125c';
+  static const clientSecret = '8bedf732ec05aa9f76d6b095a609839ed383f50c';
+
+  /// Scopes define the access for personal tokens.
+  static const scopes = ['read:user', 'repo'];
+
+  /// Url to request a user's GitHub identity
+  /// https://docs.github.com/en/apps/oauth-apps/building-oauth-apps/authorizing-oauth-apps#1-request-a-users-github-identity
+  static final authorizationEndpoint = Uri.parse(
+    'https://github.com/login/oauth/authorize',
+  );
+
+  /// Url to request user's access token
+  /// https://docs.github.com/en/apps/oauth-apps/building-oauth-apps/authorizing-oauth-apps#2-users-are-redirected-back-to-your-site-by-github
+  static final tokenEndpoint = Uri.parse(
+    'https://github.com/login/oauth/access_token',
+  );
+
+  /// redirect url after authorization, only redirect for web platform
+  static final redirectUrl = Uri.parse('http://locahost:3000/callback');
+
   final CredentialsStorage _credentialsStorage;
 
   GithubAuthenticator(this._credentialsStorage);
