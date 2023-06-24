f7bb0f227f9674b7657ac758702e3c0d729c2e5a
hwangblood <hwangblood@gmail.com>
2023-06-24 19:14:14 +0800

    Starting With the Auth Infrastructure Layer

---
+++ lib/auth/infrastructure/github_authenticator.dart
@@ -0,0 +1,8 @@
+import 'package:oauth2/oauth2.dart';
+
+class GithubAuthenticator {
+  // get the signed in credentials, it hold the access token
+  Future<Credentials> getSignedInCredentials() async {
+    throw UnimplementedError('Unimplemented: getSignedInCredentials');
+  }
+}
